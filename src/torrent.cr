0d8ba3492dccd8052980cff3106b5cf5c3d65ace
David Blurton <david.blurton@gmail.com>
2017-08-08 18:09:48 +0000

    Get list of peers from tracker

--- src/torrent.cr
+++ src/torrent.cr
@@ -14,9 +14,21 @@ module Torrent
   response.body
 
   parser = Parser.new response.body
-  puts parser.decode().pretty_inspect()
+  tf = parser.decode()
 
   info = parser.info()
 
   digest = Digest::SHA1.digest(info)
+
+  bs = digest.map {|d| d.chr.ascii? ? URI.escape(d.chr.to_s) : "%#{d.to_s(16)}"}.join
+
+  url = case tf
+    when Hash
+    tf["announce"].to_s
+  end
+
+  if url
+  	endpoint = "#{url}?info_hash=#{bs}&peer_id=torrlang10axeltest10&port=6881&compact=1&downloaded=0&event=started&uploaded=0"
+    HTTP::Client.get endpoint
+  end
 end
